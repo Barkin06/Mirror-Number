7251b0cd43f89bf45de88ecc1115835fd05edefe
Barkin06 <123365592+Barkin06@users.noreply.github.com>
2023-12-29 12:57:42 +0300

    Add files via upload

--- mirror.py
+++ mirror.py
@@ -1,80 +1,34 @@
-Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+import tkinter as tk
 
-=================== RESTART: D:/Program Files/Python310/A.py ===================
-Traceback (most recent call last):
-  File "D:/Program Files/Python310/A.py", line 21, in <module>
-    button = tk.Label(root, text="Check", command=mirror)
-  File "D:\Program Files\Python310\lib\tkinter\__init__.py", line 3177, in __init__
-    Widget.__init__(self, master, 'label', cnf, kw)
-  File "D:\Program Files\Python310\lib\tkinter\__init__.py", line 2601, in __init__
-    self.tk.call(
-_tkinter.TclError: unknown option "-command"
+def mirror():
+    num1 = userinput1.get()
+    num2 = userinput2.get()
 
-=================== RESTART: D:/Program Files/Python310/A.py ===================
-Traceback (most recent call last):
-  File "D:/Program Files/Python310/A.py", line 22, in <module>
-    button["command"] = mirror
-  File "D:\Program Files\Python310\lib\tkinter\__init__.py", line 1686, in __setitem__
-    self.configure({key: value})
-  File "D:\Program Files\Python310\lib\tkinter\__init__.py", line 1675, in configure
-    return self._configure('configure', cnf, kw)
-  File "D:\Program Files\Python310\lib\tkinter\__init__.py", line 1665, in _configure
-    self.tk.call(_flatten((self._w, cmd)) + self._options(cnf))
-_tkinter.TclError: unknown option "-command"
+    if len(num1) == 2 and len(num2) == 2 and num1[1] == num2[0] and num1[0] == num2[1]:
+        result.config(text="Mirror")
+    else:
+        result.config(text="Not mirror")
 
-=================== RESTART: D:/Program Files/Python310/A.py ===================
-Traceback (most recent call last):
-  File "D:/Program Files/Python310/A.py", line 21, in <module>
-    button = tk.Label(root, text="Check",command=mirror)
-  File "D:\Program Files\Python310\lib\tkinter\__init__.py", line 3177, in __init__
-    Widget.__init__(self, master, 'label', cnf, kw)
-  File "D:\Program Files\Python310\lib\tkinter\__init__.py", line 2601, in __init__
-    self.tk.call(
-_tkinter.TclError: unknown option "-command"
+root = tk.Tk()
+root.title("Mirror check")
 
-============================================================= RESTART: D:/Program Files/Python310/A.py ============================================================
-Traceback (most recent call last):
-  File "D:/Program Files/Python310/A.py", line 21, in <module>
-    button = tk.button(root, text="Check",command=mirror)
-AttributeError: module 'tkinter' has no attribute 'button'. Did you mean: 'Button'?
->>> 
-============================================================= RESTART: D:/Program Files/Python310/A.py ============================================================
-Exception in Tkinter callback
-Traceback (most recent call last):
-  File "D:\Program Files\Python310\lib\tkinter\__init__.py", line 1921, in __call__
-    return self.func(*args)
-  File "D:/Program Files/Python310/A.py", line 4, in mirror
-    num1 = userinput1.get()
-NameError: name 'userinput1' is not defined
-Exception in Tkinter callback
-Traceback (most recent call last):
-  File "D:\Program Files\Python310\lib\tkinter\__init__.py", line 1921, in __call__
-    return self.func(*args)
-  File "D:/Program Files/Python310/A.py", line 4, in mirror
-    num1 = userinput1.get()
-NameError: name 'userinput1' is not defined
->>> 
-============================================================= RESTART: D:/Program Files/Python310/A.py ============================================================
-Exception in Tkinter callback
-Traceback (most recent call last):
-  File "D:\Program Files\Python310\lib\tkinter\__init__.py", line 1921, in __call__
-    return self.func(*args)
-  File "D:/Program Files/Python310/A.py", line 4, in mirror
-    num1 = userinput1.get()
-AttributeError: 'Label' object has no attribute 'get'
-Exception in Tkinter callback
-Traceback (most recent call last):
-  File "D:\Program Files\Python310\lib\tkinter\__init__.py", line 1921, in __call__
-    return self.func(*args)
-  File "D:/Program Files/Python310/A.py", line 4, in mirror
-    num1 = userinput1.get()
-AttributeError: 'Label' object has no attribute 'get'
->>> 
-============================================================= RESTART: D:/Program Files/Python310/A.py ============================================================
->>> 
-============================================================= RESTART: D:/Program Files/Python310/A.py ============================================================
->>> 
-============================================================= RESTART: D:/Program Files/Python310/A.py ============================================================
->>> 
-============================================================= RESTART: D:/Program Files/Python310/A.py ============================================================
+x = tk.Label(root, text="Enter number")
+x.pack()
+
+
+userinput1 = tk.Entry(root)
+userinput1.pack()
+
+userinput2 = tk.Entry(root)
+userinput2.pack()
+
+result = tk.Label(root, text="")
+result.pack()
+
+
+
+button = tk.Button(root, text="Check",command=mirror)
+button.pack()
+
+
+root.mainloop()
